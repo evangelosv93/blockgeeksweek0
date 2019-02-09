2aa24dfac390b477d72b87d5bd204986257834f6
Evangelos Voultepsis <33362678+evangelosv93@users.noreply.github.com>
2019-02-09 12:23:01 -0500

    Create conditionalSums.js

---
+++ conditionalSums.js
@@ -0,0 +1,23 @@
+var conditionalSum = function(values, condition) {
+  let result = 0;
+
+  if (condition === "odd") {
+    remainder = 1;
+  } else {
+    remainder = 0;
+  }
+
+//iterator
+  for (i = 0; i < values.length; i++) {
+    if ((values[i] % 2) === remainder) {
+      result += values[i];
+    }
+  }
+
+  return result;
+};
+
+console.log(conditionalSum([1, 2, 3, 4, 5], "even"));
+console.log(conditionalSum([1, 2, 3, 4, 5], "odd"));
+console.log(conditionalSum([13, 88, 12, 44, 99], "even"));
+console.log(conditionalSum([], "odd"));
